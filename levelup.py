72fe807cd40f510de7272ca94b3419e6635790d1
Victor <139696656+VictorAlexisL@users.noreply.github.com>
2023-09-25 22:58:34 -0300

    Add files via upload
    Simulador de level up usando classes e métodos de classe

---
+++ levelup.py
@@ -0,0 +1,55 @@
+character_list = []
+
+class Character:
+    def __init__(self, name, character_class):
+        self.name = name
+        self.level = 0
+        self.exp = 0
+        self.character_class = character_class
+        self.strength = 10
+        self.intelligence = 10
+        self.dexterity = 10
+        print(self.name)
+        character_list.append(self.name)
+
+    def get_exp(self):
+        import random
+        self.exp += random.randint(1,100)
+        if self.exp >= 100:
+            self.level_up()        
+        return self.exp
+    
+    def level_up(self):
+        self.level += 1
+        if self.character_class == "Warrior":
+            import random
+            self.strength += random.randint(1,2)
+            self.intelligence += random.randint(0,1)
+            self.dexterity += random.randint(0,2)
+        elif self.character_class == "Wizard":
+            import random
+            self.strength += random.randint(0,1)
+            self.intelligence += random.randint(1,2)
+            self.dexterity += random.randint(0,2)
+        else:
+            import random
+            self.strength += random.randint(0,2)
+            self.intelligence += random.randint(0,1)
+            self.dexterity += random.randint(1,2)
+        print(f"Level Up! Current level: {self.level}")
+        return self.level
+    
+    def debug(self):
+        while self.level < 5:
+            Character.get_exp(self)
+        print(self.strength)
+        print(self.intelligence)
+        print(self.dexterity)
+        print("Done testing.")
+        return True
+    
+merlin = Character("Merlin", "Wizard")
+
+Character.debug(merlin)
+
+print(character_list)
